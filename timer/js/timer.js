f58818e0795d61a808e7dfcfcfcce837a77bd9f6
Литвиновский Алексей Борисович <LITVINOVSKIY-AB@sibir.FSK-EES.LOCAL>
2021-12-23 15:54:05 +0700

    1

---
+++ timer/js/timer.js
@@ -0,0 +1,33 @@
+import { DateTime } from 'https://moment.github.io/luxon/es6/luxon.min.js';
+import { getDateDiffNowString } from '../../common/js/getDateDiff.js';
+import '../../common/js/howler.js';
+
+const Timer = () => {
+    let tm = null;
+
+    const sound = new Howl({
+        src: ['./media/zing_1.mp3'],
+    });
+
+    return {
+        start(timeStr, cb) {
+            let arr = timeStr.split(':');
+            let initialDate = DateTime.now();
+            initialDate = initialDate.set({ hour: initialDate.hour + +arr[0], minute: initialDate.minute + +arr[1], second: initialDate.second + +arr[2] });
+
+            tm = setInterval(() => {
+                const str = getDateDiffNowString(initialDate);
+                if (str.length === 0) {
+                    this.stop();
+                }
+                cb(str);
+            }, 1000);
+        },
+        stop() {
+            clearInterval(tm);
+            sound.play();
+        },
+    }
+}
+
+window.tm = Timer();
